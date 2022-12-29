58d204ffc55c14f86768cd9e4aab383e21e07aee
Gabriel Holz <gabriegustavogramsholz@gmail.com>
2022-12-29 07:56:14 -0300

    adicionado metodos

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
      res.header("Access-Control-Allow-Origin", "*");
 	// //Quais são os métodos que a conexão pode realizar na API
-    // res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
      app.use(cors(corsOptions));
     console.log('Acesso!!')
      next();
@@ -42,7 +42,7 @@ app.use(bodyParser.json());
 app.post("/resultadomegasena", cors(), async (req, res, next) => {
   // console.log(req.body)
   // res.send(JSON.stringify(`Com o valor de ${req.body.price} você consegue comprar várias coisas`));
-  
+
   let data = {};
 
   await scrapingLastNumbersMegaSena().then((response) => {
