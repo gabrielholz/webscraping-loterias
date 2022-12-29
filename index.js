05e04d7d8e60fcd15bbdb8b5cb51ed3276fc71fe
Gabriel Holz <gabriegustavogramsholz@gmail.com>
2022-12-29 06:06:28 -0300

    ajustes 1

--- index.js
+++ index.js
@@ -9,39 +9,43 @@ import cors from "cors";
 import { scrapingLastNumbersMegaSena } from "./functions/getMegaSena.js";
 import { headers } from "./configHeader.js";
 
-const cors = require('cors');
+// const cors = require('cors');
 // Seus domínios permitidos devem estar aqui
-const allowedOrigins = ['http://localhost:3000','https://megasena.devholz.com/', "http://10.0.0.22:3000"]; 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://megasena.devholz.com/",
+  "http://10.0.0.22:3000",
+];
 const corsOption = {
-    origin: allowedOrigins,
-    
+  origin: allowedOrigins,
 };
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(headers())
-
-
+// app.use(headers)
 
 //Routes
-app.post("/getmegasena",async (req, res) => {
-    
+app.post("/getmegasena", async (req, res) => {
   // console.log(req.body)
   // res.send(JSON.stringify(`Com o valor de ${req.body.price} você consegue comprar várias coisas`));
-    let data = {}
+  let data = {};
 
- await scrapingLastNumbersMegaSena().then((response) => {
-    data = {response}
+  await scrapingLastNumbersMegaSena().then((response) => {
+    data = { response };
   });
 
   res.send(JSON.stringify(data));
 });
 
+app.get("/getteste", async (req, res) => {
+  let data = { name: "Gabriel", company: "devholz" };
 
+  return res.json(data);
+});
 
 //Start server
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 3009;
 app.listen(port, (req, res) => {
   console.log("Servidor Rodando ", port);
 });
