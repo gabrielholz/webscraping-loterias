6bbf79ad49533d5727bfeabd0296bcc453232ea4
Gabriel Holz <gabriegustavogramsholz@gmail.com>
2022-12-29 05:53:46 -0300

    ajustes

--- index.js
+++ index.js
@@ -7,22 +7,26 @@ import bodyParser from "body-parser";
 import cors from "cors";
 // const cors = require('cors');
 import { scrapingLastNumbersMegaSena } from "./functions/getMegaSena.js";
-
+import { headers } from "./configHeader.js";
 
 const cors = require('cors');
 // Seus domínios permitidos devem estar aqui
 const allowedOrigins = ['http://localhost:3000','https://megasena.devholz.com/', "http://10.0.0.22:3000"]; 
 const corsOption = {
     origin: allowedOrigins,
+    
 };
 const app = express();
-app.use(cors(corsOption));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(headers())
+
 
 
 //Routes
 app.post("/getmegasena",async (req, res) => {
+    
   // console.log(req.body)
   // res.send(JSON.stringify(`Com o valor de ${req.body.price} você consegue comprar várias coisas`));
     let data = {}
