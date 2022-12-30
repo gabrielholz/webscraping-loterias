5f83a68b645b695949b192b3cfd3b81ee8e4ddf8
Gabriel Holz <gabriegustavogramsholz@gmail.com>
2022-12-29 22:12:17 -0300

    alteracao em cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const allowedOrigins = [
   "http://10.0.0.22:3000",
 ];
 let corsOptions = {
-    origin: false,
+    origin: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   }
 
@@ -25,7 +25,7 @@ const app = express();
 // app.use(cors(corsOptions));
 app.use((req, res, next) => {
 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    //  res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Origin", "*");
 	// //Quais são os métodos que a conexão pode realizar na API
     // res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
      app.use(cors(corsOptions));
